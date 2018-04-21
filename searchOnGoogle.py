9e923164264cbc0d49f5fa05d4400833075e8b69
leica19 <spike-19@ezweb.ne.jp>
2018-04-21 16:14:12 +0900

    set the const.

--- searchOnGoogle.py
+++ searchOnGoogle.py
@@ -1,20 +1,19 @@
+
 import requests, sys, webbrowser, bs4
 
 GOOGLE_DOMAIN = 'http://google.com/'
+TAB_NUM = 5
 
 print('goo...')
 
 res = requests.get(GOOGLE_DOMAIN + 'search?q=' + ''.join(sys.argv[1:]))
 
+# print the status code
 print(res)
 
 soup = bs4.BeautifulSoup(res.text, 'lxml')
-
 elems = soup.select('.r a')
 
-print(elems)
-
-num_open = min(10, len(elems))
-
+num_open = min(TAB_NUM, len(elems))
 for i in range(num_open):
     webbrowser.open(GOOGLE_DOMAIN + elems[i].get('href'))
