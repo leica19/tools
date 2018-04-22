1aed23cd47fb9056001860127f916e2c5c3e0b9b
leica19 <spike-19@ezweb.ne.jp>
2018-04-22 11:32:58 +0900

    da

--- getPicturesOnGoogle.py
+++ getPicturesOnGoogle.py
@@ -10,20 +10,20 @@ if not os.path.exists(keyword):
 
 urlKeyword = parse.quote(keyword)
 url = 'https://www.google.com/search?hl=jp&q=' + urlKeyword + '&btnG=Google+Search&tbs=0&safe=off&tbm=isch'
-
 headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",}
+
 request = req.Request(url=url, headers=headers)
 page = req.urlopen(request)
 
 html = page.read().decode('utf-8')
 html = bs4.BeautifulSoup(html, "html.parser")
 elems = html.select('.rg_meta.notranslate')
-    
-for ele in elems:
+
+for elem in elems:
     print("画像のダウンロード中...")
-    ele = ele.contents[0].replace('"','').split(',')
+    elem = elem.contents[0].replace('"','').split(',')
     eledict = dict()
-    for e in ele:
+    for e in elem:
         num = e.find(':')
         eledict[e[0:num]] = e[num+1:]
     imageURL = eledict['ou']
@@ -55,4 +55,3 @@ for ele in elems:
         continue
     except error.URLError:
         continue
-
