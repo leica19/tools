8e751645682724ac905092201dc8d6e2521b50db
leica19 <spike-19@ezweb.ne.jp>
2018-04-22 11:18:58 +0900

    implemented: argv2

--- searchOnGoogleRoughly.py
+++ searchOnGoogleRoughly.py
@@ -2,11 +2,16 @@
 import requests, sys, webbrowser, bs4
 
 GOOGLE_DOMAIN = 'http://google.com/'
-TAB_NUM = 5
-
-print('goo...')
 
 res = requests.get(GOOGLE_DOMAIN + 'search?q=' + ''.join(sys.argv[1:]))
+
+try:
+    if sys.argv[2]:
+        tabNum = int(sys.argv[2])
+except:
+    tabNum = 5
+
+print('goo...')
 
 # print the status code
 print(res)
@@ -14,6 +19,6 @@ print(res)
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 elems = soup.select('.r a')
 
-num_open = min(TAB_NUM, len(elems))
+num_open = min(tabNum, len(elems))
 for i in range(num_open):
     webbrowser.open(GOOGLE_DOMAIN + elems[i].get('href'))
